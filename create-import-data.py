069cfe95d47f958ba9f88f850c14541d843760f2
Jussi Judin <jjudin+bitbucket@iki.fi>
2011-07-28 21:48:27 +0300

    Take winter events into account.

--- create-import-data.py
+++ create-import-data.py
@@ -104,7 +104,7 @@ def print_entry(year, entry):
     if position != 0:
         position_str = str(position) + endings.get(position, 'th') + " place"
 
-    if not "AssemblyTV" in section_name:
+    if not "AssemblyTV" in section_name and not "Winter" in section_name:
         if not "Seminars" in section_name:
             if position_str is not None:
                 description += u"<p>%s" % position_str
