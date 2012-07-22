2758ee8ba783c8acde6b07d9ec595f999d696365
Jussi Judin <jjudin+bitbucket@iki.fi>
2012-07-22 21:26:18 +0300

    Add script to upload public videos to Youtube.

---
+++ upload-to-youtube-video.py
@@ -0,0 +1,156 @@
+import asmmetadata
+import os.path
+import sys
+import subprocess
+import time
+
+email = sys.argv[1]
+password = sys.argv[2]
+files_root = sys.argv[3]
+video_postfix = sys.argv[4]
+
+yearline = sys.stdin.readline().strip()
+
+print yearline
+
+data_type, year = yearline.split(" ", 1)
+assert data_type == ":year"
+
+failures = 0
+
+zero_position = 1
+
+for line in sys.stdin:
+    sys.stdout.flush()
+    # Fast-forward if there are over 2 consecutive failures.
+    # Youtube is probably blocking then and we need to wait 10 minutes.
+    if failures > 2:
+        sys.stdout.write(line)
+        continue
+
+    try:
+        line = unicode(line.strip(), "utf-8")
+    except ValueError, e:
+        # Why is this happening?
+        sys.stdout.write(line)
+        continue
+
+    if line == "":
+        print
+        continue
+
+    if line[0] == "#":
+        print line.encode("utf-8")
+        continue
+
+    if line[0] == ":":
+        print line.encode('utf-8')
+        data_type, value = line.split(" ", 1)
+        if data_type == ":section":
+            zero_position = 1
+            section = value
+        continue
+
+    entryinfo = asmmetadata.parse_entry_line(line)
+    entryinfo['section'] = {
+        'name': section,
+        'year': int(year),
+        }
+
+    author = entryinfo.get("author", None)
+    title = entryinfo.get("title", None)
+    if title is None or author is None:
+        sys.stderr.write(("FAILED to get author or title %s\n" % line).encode('utf-8'))
+        print line.encode('utf-8')
+        continue
+
+    title = title.replace("<", "-").replace(">", "-")
+    author = author.replace("<", "-").replace(">", "-")
+
+    position = entryinfo.get('position', None)
+    if position is None:
+        position_filename = "9%02d" % zero_position
+    else:
+        position_filename = "%02d" % position
+
+    source_file_base = asmmetadata.normalize_key(
+        "%s-%s-%s-%s-by-%s" % (
+            year,
+            section,
+            position_filename,
+            title,
+            author)
+        )
+    source_file = os.path.join(files_root, year, source_file_base + video_postfix)
+
+    if not os.path.exists(source_file):
+        print line.encode('utf-8')
+        continue
+
+    if position is None:
+        zero_position += 1
+
+    video_file = source_file
+
+    youtube_data = asmmetadata.get_youtube_info_data(entryinfo)
+    youtube_title = youtube_data['title']
+    description = youtube_data['description']
+    category = youtube_data['category']
+    tag_list = youtube_data['tags']
+
+    if os.path.getsize(source_file) > (2**31 - 1):
+        if 'youtube' not in entryinfo:
+            print "# XXX not-uploaded over 2GB file!"
+        print line.encode('utf-8')
+        sys.stderr.write("# XXX not-uploaded over 2GB file!\n")
+        sys.stderr.write("%s\n" % youtube_title)
+        sys.stderr.write("%s\n" % description)
+        sys.stderr.write("%s\n" % category)
+        sys.stderr.write("%s\n" % ", ".join(tag_list))
+        continue
+
+    if 'youtube' in entryinfo:
+        print line.encode('utf-8')
+        continue
+
+    tags = ",".join(tag_list)
+
+    upload_trials = 0
+    args = ['youtube-upload', '--no-split', '--', email, password, video_file, youtube_title, description, category, tags]
+    upload_success = False
+    youtube_id = ''
+    # 3 trials to upload video with one extra retry chance.
+    while upload_success == False and upload_trials < 3:
+        if upload_trials == 2:
+            sys.stderr.write("YOUTUBE is blocking, sleeping for 10 minutes!\n")
+            sys.stderr.write("%s\n" % time.strftime("%H:%M:%S"))
+            # Youtube is probably blocking and we need to wait for 10 minutes.
+            time.sleep(601)
+        upload_trials += 1
+        p = subprocess.Popen(args, stdout=subprocess.PIPE)
+        output, errors = p.communicate()
+        outlines = output.strip().split("\n")
+        if 'youtube.com' in outlines[-1]:
+            upload_success = True
+            youtube_http_id = outlines[-1]
+            youtube_id = "|youtube:" + youtube_http_id.replace("https://www.youtube.com/watch?v=", "")
+            failures = 0
+        else:
+            sys.stderr.write(("UPLOAD failed %s\n" % line).encode('utf-8'))
+            sys.stderr.write("\n".join(outlines))
+            sys.stderr.write(youtube_title.encode('utf-8'))
+            sys.stderr.write("\n")
+            sys.stderr.write(description.encode('utf-8'))
+            sys.stderr.write("\n")
+            sys.stderr.write(tags)
+            sys.stderr.write("\n")
+
+    if upload_success:
+        failures = 0
+    else:
+        failures += 1
+    print (line + youtube_id).encode('utf-8')
+    sys.stdout.flush()
+    sys.stderr.write("done\n")
+    # 61 seconds delay between sends is OK, 57 is not.
+    time.sleep(61)
