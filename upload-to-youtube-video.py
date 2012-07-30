61d016eec11f8b7166f367915404074a63047edb
Jussi Judin <jjudin+bitbucket@iki.fi>
2012-07-30 19:04:53 +0300

    Add dry-run option for Youtube uploading for easier data generation.

--- upload-to-youtube-video.py
+++ upload-to-youtube-video.py
@@ -1,13 +1,34 @@
+import argparse
 import asmmetadata
 import os.path
 import sys
 import subprocess
 import time
 
-email = sys.argv[1]
-password = sys.argv[2]
-files_root = sys.argv[3]
-video_postfix = sys.argv[4]
+parser = argparse.ArgumentParser(description='Upload videos to Youtube.')
+parser.add_argument('email')
+parser.add_argument('password')
+parser.add_argument('files_root', metavar="files-root")
+parser.add_argument('--video-postfix', default=".mp4")
+parser.add_argument('--dry-run', action="store_true")
+commandline_args = parser.parse_args(sys.argv[1:])
+
+email = commandline_args.email
+password = commandline_args.password
+files_root = commandline_args.files_root
+video_postfix = commandline_args.video_postfix
+
+def call_and_capture_output(args):
+    p = subprocess.Popen(args, stdout=subprocess.PIPE)
+    output, errors = p.communicate()
+    outlines = output.strip().split("\n")
+    return outlines
+
+sleep_function = time.sleep
+if commandline_args.dry_run:
+    sleep_function = lambda x : None
+    def call_and_capture_output(args):
+        return [""]
 
 yearline = sys.stdin.readline().strip()
 
@@ -98,19 +119,18 @@ for line in sys.stdin:
     category = youtube_data['category']
     tag_list = youtube_data['tags']
 
+    if 'youtube' in entryinfo:
+        print line.encode('utf-8')
+        continue
+
     if os.path.getsize(source_file) > (2**31 - 1):
-        if 'youtube' not in entryinfo:
-            print "# XXX not-uploaded over 2GB file!"
+        print "# XXX not-uploaded over 2GB file!"
         print line.encode('utf-8')
         sys.stderr.write("# XXX not-uploaded over 2GB file!\n")
         sys.stderr.write("%s\n" % youtube_title)
         sys.stderr.write("%s\n" % description)
         sys.stderr.write("%s\n" % category)
         sys.stderr.write("%s\n" % ", ".join(tag_list))
-        continue
-
-    if 'youtube' in entryinfo:
-        print line.encode('utf-8')
         continue
 
     tags = ",".join(tag_list)
@@ -125,11 +145,9 @@ for line in sys.stdin:
             sys.stderr.write("YOUTUBE is blocking, sleeping for 10 minutes!\n")
             sys.stderr.write("%s\n" % time.strftime("%H:%M:%S"))
             # Youtube is probably blocking and we need to wait for 10 minutes.
-            time.sleep(601)
+            sleep_function(601)
         upload_trials += 1
-        p = subprocess.Popen(args, stdout=subprocess.PIPE)
-        output, errors = p.communicate()
-        outlines = output.strip().split("\n")
+        outlines = call_and_capture_output(args)
         if 'youtube.com' in outlines[-1]:
             upload_success = True
             youtube_http_id = outlines[-1]
@@ -153,4 +171,4 @@ for line in sys.stdin:
     sys.stdout.flush()
     sys.stderr.write("done\n")
     # 61 seconds delay between sends is OK, 57 is not.
-    time.sleep(61)
+    sleep_function(61)
