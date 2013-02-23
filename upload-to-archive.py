4dfcdaca957a6bff85c15824452c2d3bb5303fb2
Jussi Judin <jjudin+bitbucket@iki.fi>
2013-02-23 20:12:50 +0200

    Archive upload script.

---
+++ upload-to-archive.py
@@ -0,0 +1,17 @@
+import argparse
+import requests
+
+parser = argparse.ArgumentParser()
+parser.add_argument("upload_page")
+parser.add_argument("upload_account")
+parser.add_argument("upload_password")
+parser.add_argument("import_file", type=argparse.FileType("rb"))
+args = parser.parse_args()
+
+result = requests.post(
+    args.upload_page,
+    data={"form.actions.import": "Import"},
+    files={"form.data": args.import_file},
+    auth=(args.upload_account, args.upload_password))
+
+print result.code
