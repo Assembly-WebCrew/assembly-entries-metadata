32d6a681a74efe785d39bd2e8d574b39e4ca09af
Jussi Judin <jjudin+bitbucket@iki.fi>
2013-08-01 20:27:58 +0300

    Add PMS category merging script

---
+++ pms-merge-category-data.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+source "$(dirname "$0")"/variables.inc
+
+COMPO_CATEGORY="$1"
+if test -z "$COMPO_CATEGORY"; then
+    echo No compo category given.
+    exit 1
+fi
+
+MERGE_TEMPFILE="$DATAFILE"-merged.txt
+
+$PYTHON "$SCRIPTDIR"/merge-data-pms-archive.py "$DATAFILE" "$PMS_LOGIN" "$PMS_PASSWORD" "$COMPO_CATEGORY" > \
+    "$MERGE_TEMPFILE" && cat "$MERGE_TEMPFILE" "$DATAFILE"
