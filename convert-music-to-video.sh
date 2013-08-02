e6141c3e6ee806cb060b17dce95c0c7c4d288c86
Jussi Judin <jjudin+bitbucket@iki.fi>
2013-08-02 21:34:48 +0300

    Remove broken Youtube video links from music files

--- convert-music-to-video.sh
+++ convert-music-to-video.sh
@@ -20,5 +20,5 @@ for MUSIC_FILE in "$CATEGORY"/{*.wav,*.ogg,*.mp3}; do
     mplayer -ao pcm:file="$IDFILE" "$MUSIC_FILE"
     LENGTH=$(mplayer -ao null -vo null -frames 0 -identify -really-quiet "$IDFILE" | grep ID_LENGTH | cut -f 2 -d =)
 
-    mencoder -fps 1/"$LENGTH" -ofps 30 -o "$TARGET_FILE" -ovc lavc -oac copy -audiofile "$MUSIC_FILE" mf://"$MUSIC_BACKGROUND"
+    mencoder -fps 1/"$LENGTH" -ofps 30 -o "$TARGET_FILE" -ovc lavc -oac copy -audiofile "$IDFILE" mf://"$MUSIC_BACKGROUND"
 done
