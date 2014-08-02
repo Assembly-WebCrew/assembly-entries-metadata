67eca09a37a2252d4d805de77b60d3f56b2a5f84
Jussi Judin <jjudin+bitbucket@iki.fi>
2014-08-02 23:25:50 +0300

    Skip unpublished results in compo data parsing

--- lib/pms-parse-dump-compo-places.py
+++ lib/pms-parse-dump-compo-places.py
@@ -9,21 +9,25 @@ args = parser.parse_args()
 
 pms_dom = xml.dom.minidom.parse(open(args.pms_datafile, "rb"))
 
-entries = pms_dom.getElementsByTagName("entry")
-
 iddata = {}
 
-for entry in entries:
-    entry_id = entry.getAttribute("id")
-    entry_position_str = entry.getAttribute("place") or '0'
-    techniques = entry.getAttribute("techniques")
-    if len(techniques) == 0:
-        techniques = None
-    platform = entry.getAttribute("platform")
-    if len(platform) == 0:
-        platform = None
-    entry_position = int(entry_position_str) or None
-    iddata[entry_id] = (entry_position, techniques, platform)
+compos = pms_dom.getElementsByTagName("compo")
+
+for compo in compos:
+    if compo.getAttribute("resultspublic") != "True":
+        continue
+    entries = compo.getElementsByTagName("entry")
+    for entry in entries:
+        entry_id = entry.getAttribute("id")
+        entry_position_str = entry.getAttribute("place") or '0'
+        techniques = entry.getAttribute("techniques")
+        if len(techniques) == 0:
+            techniques = None
+        platform = entry.getAttribute("platform")
+        if len(platform) == 0:
+            platform = None
+        entry_position = int(entry_position_str) or None
+        iddata[entry_id] = (entry_position, techniques, platform)
 
 import asmmetadata
 
@@ -33,6 +37,9 @@ for entry in parsed.entries:
     if 'pms-id' not in entry:
         continue
     _, _, pms_id = entry['pms-id'].split("/")
+    if pms_id not in iddata:
+        # This probably means that results of a category are not public.
+        continue
     place, techniques, platform = iddata[pms_id]
     entry['position'] = place
     if techniques is not None:
