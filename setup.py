c0cd708be235ba15da7f6ee4256b4efc8b279aa3
Jussi Judin <jjudin+asmmetadata@iki.fi>
2014-08-08 01:45:39 +0300

    Add buildout based interpreter

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(name='assembly-entries-metadata',
+      version='1.0.0dev',
+      description="Assembly competition entries metadata",
+      author="Assembly Webcrew",
+      author_email="web@assembly.org",
+      url="http://archive.assembly.org/",
+      license="AGPLv3",
+      install_requires=['gdata==2.0.18'])
