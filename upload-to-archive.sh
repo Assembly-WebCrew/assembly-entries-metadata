c45e9bf57a2015419bc72ffde938ddf35e509b10
Jussi Judin <jjudin+asmmetadata@iki.fi>
2015-07-31 11:21:11 +0300

    Make upload-to-archive to use default parameter that works with set -u

--- upload-to-archive.sh
+++ upload-to-archive.sh
@@ -4,9 +4,6 @@ set -eu
 
 source "$(dirname "$0")"/variables.inc.sh
 
-IMPORT_FILE="$1"
-if test -z "$IMPORT_FILE"; then
-    IMPORT_FILE="$OUTFILE"
-fi
+IMPORT_FILE="${1:-$OUTFILE}"
 
 $PYTHON "$SCRIPTDIR"/upload-to-archive.py "$WEB_UPLOAD_PAGE" "$WEB_UPLOAD_ACCOUNT" "$WEB_UPLOAD_PASSWORD" "$IMPORT_FILE"
