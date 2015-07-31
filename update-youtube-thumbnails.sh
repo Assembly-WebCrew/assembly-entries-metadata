1b531d1bf80e341e59624ec34d0721c996c59cfe
Jussi Judin <jjudin+asmmetadata@iki.fi>
2015-07-31 16:21:36 +0300

    Always create Youtube thumbnails directory

--- update-youtube-thumbnails.sh
+++ update-youtube-thumbnails.sh
@@ -4,4 +4,6 @@ set -eu
 
 source "$(dirname "$0")"/variables.inc.sh
 
+mkdir -p "$FILES_ROOT"/youtube-thumbnails
+
 $PYTHON "$SCRIPTDIR"/update-youtube-thumbnails.py "$FILES_ROOT"/youtube-thumbnails 160 90 < "$DATAFILE"
