f013d64a5011738a286eb6013d1925f0b1da4538
Jussi Judin <jjudin+asmmetadata@iki.fi>
2016-08-05 16:38:42 +0300

    Make thumbnail conversion less trashing

--- thumbnail-convert.sh
+++ thumbnail-convert.sh
@@ -5,6 +5,12 @@ set -eu
 source "$(dirname "$0")"/variables.inc.sh
 
 cd "$FILES_ROOT" || exit 1
+RESIZED_NAME=$(mktemp)
+function cleanup()
+{
+    rm -f "$RESIZED_NAME"
+}
+trap cleanup EXIT
 
 for D in "$@" ; do
     echo "$D"
@@ -21,10 +27,10 @@ for D in "$@" ; do
             convert -resize 640x10000 "$I" thumbnails/large/"$D"/"$BASE".png
             zopflipng -m thumbnails/large/"$D"/"$BASE".png thumbnails/large/"$D"/"$BASE".png.z && mv thumbnails/large/"$D"/"$BASE".png.z thumbnails/large/"$D"/"$BASE".png
 
-            convert "$I" -resize 160x5000 resized.png
-            convert -gravity Center -crop 160x90+0+0 +repage resized.png thumbnails/small/"$D"/"$BASE".jpeg
+            convert "$I" -resize 160x5000 "$RESIZED_NAME"
+            convert -gravity Center -crop 160x90+0+0 +repage "$RESIZED_NAME" thumbnails/small/"$D"/"$BASE".jpeg
             jpegoptim --strip-all thumbnails/small/"$D"/"$BASE".jpeg
-            convert -gravity Center -crop 160x90+0+0 +repage resized.png thumbnails/small/"$D"/"$BASE".png
+            convert -gravity Center -crop 160x90+0+0 +repage "$RESIZED_NAME" thumbnails/small/"$D"/"$BASE".png
             zopflipng -m thumbnails/small/"$D"/"$BASE".png thumbnails/small/"$D"/"$BASE".png.z && mv thumbnails/small/"$D"/"$BASE".png.z thumbnails/small/"$D"/"$BASE".png
         done
     done
