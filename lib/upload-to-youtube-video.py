10b9c8a7996467fffe6040fb42e0630d9b89dd8d
Jussi Judin <jjudin+github@iki.fi>
2018-08-03 20:21:15 +0300

    Make Youtube uploads work again

--- lib/upload-to-youtube-video.py
+++ lib/upload-to-youtube-video.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import argparse
 import asmmetadata
+import asmyoutube
 import os.path
 import re
 import sys
@@ -11,20 +12,6 @@ ABORT_FAILURES = 2
 UPLOAD_TRIALS = 3
 # 10 minutes is the definite minimum sleeping time in Youtube for retries.
 YOUTUBE_MINIMUM_SLEEP_TIME = 601
-
-parser = argparse.ArgumentParser(description='Upload videos to Youtube.')
-parser.add_argument('files_root', metavar="files-root")
-parser.add_argument('--video-postfix', default=".mp4")
-parser.add_argument('--dry-run', action="store_true")
-parser.add_argument('--media-vod-directory')
-parser.add_argument(
-    '--privacy', default="public", choices=["public", "unlisted", "private"])
-commandline_args = parser.parse_args(sys.argv[1:])
-
-files_root = commandline_args.files_root
-video_postfix = commandline_args.video_postfix
-media_vod_directory = commandline_args.media_vod_directory
-privacy = commandline_args.privacy
 
 
 def call_and_capture_output_real(args):
@@ -39,121 +26,55 @@ def call_and_capture_output_real(args):
 def call_and_capture_output_fake(args):
     return ["http://www.youtube.com/watch?v=asdf"]
 
-sleep_function = time.sleep
-if commandline_args.dry_run:
-    sleep_function = lambda x : None
-    call_and_capture_output = call_and_capture_output_fake
-else:
-    call_and_capture_output = call_and_capture_output_real
 
-yearline = sys.stdin.readline().strip()
+class State:
+    def __init__(self, files_root, media_vod_directory):
+        self.files_root = files_root
+        self.media_vod_directory = media_vod_directory
+        self.failures = 0
+        self.zero_position = 1
 
-print yearline
 
-data_type, year = yearline.split(" ", 1)
-assert data_type == ":year"
-
-failures = 0
-
-zero_position = 1
-
-section_data = {}
-
-for line in sys.stdin:
-    sys.stdout.flush()
+def handle_entry(
+        call_and_capture_output,
+        sleep_function,
+        privacy,
+        state,
+        entry):
+    if "youtube" in entry:
+        return
     # Fast-forward if there are many consecutive failures.
     # Youtube is probably blocking then and we need to wait 10 minutes.
-    if failures > ABORT_FAILURES:
-        sys.stdout.write(line)
-        continue
+    if state.failures > ABORT_FAILURES:
+        raise RuntimeError(
+            "Failures %d exceeded abort failures %d!",
+            state.failures, ABORT_FAILURES)
 
-    try:
-        line = unicode(line.strip(), "utf-8")
-    except ValueError, e:
-        # Why is this happening?
-        sys.stdout.write(line)
-        continue
+    source_file = "oaisdfjtTODOD"
+    # source_file_base = asmmetadata.normalize_key(
+    #     "%s-%s%s-%s-by-%s" % (
+    #         year,
+    #         section,
+    #         position_filename,
+    #         title,
+    #         author)
+    #     )
+    # source_file = os.path.join(
+    #     files_root, year, source_file_base + video_postfix)
 
-    if line == "":
-        print
-        continue
-
-    if line[0] == "#":
-        print line.encode("utf-8")
-        continue
-
-    if line[0] == ":":
-        print line.encode('utf-8')
-        data_type, value = line.split(" ", 1)
-        if data_type == ":section":
-            section_data = {
-                'name': value,
-                'year': int(year)
-                }
-            zero_position = 1
-            section = section_data['name']
-        else:
-            section_data[str(data_type.lstrip(":"))] = value
-        continue
-
-    entryinfo = asmmetadata.parse_entry_line(line)
-    entryinfo['section'] = section_data
-
-    author = entryinfo.get("author", None)
-    title = entryinfo.get("title", None)
-    if title is None or author is None:
-        sys.stderr.write(("FAILED to get author or title %s\n" % line).encode('utf-8'))
-        print line.encode('utf-8')
-        continue
-
-    title = title.replace("<", "-").replace(">", "-")
-    author = author.replace("<", "-").replace(">", "-")
-
-    position = entryinfo.get('position', None)
-    if position is None:
-        #position_filename = "9%02d" % zero_position
-        position_filename = "-99"
-    else:
-        position_filename = "-%02d" % position
-
-    if section.lower() in ["misc", "assemblytv", "winter", "seminars"]:
-        position_filename = ""
-
-    source_file_base = asmmetadata.normalize_key(
-        "%s-%s%s-%s-by-%s" % (
-            year,
-            section,
-            position_filename,
-            title,
-            author)
-        )
-    source_file = os.path.join(
-        files_root, year, source_file_base + video_postfix)
-
-    if not os.path.exists(source_file) and 'video-file' in entryinfo:
-        source_file = os.path.join(files_root, entryinfo['video-file'])
-
-    if not os.path.exists(source_file) and 'media' in entryinfo and not media_vod_directory is None:
-        source_file = os.path.join(media_vod_directory, entryinfo['media'].lstrip("/"))
+    if not os.path.exists(source_file) and 'video-file' in entry:
+        source_file = os.path.join(state.files_root, entry['video-file'])
 
     if not os.path.exists(source_file):
-        print line.encode('utf-8')
-        continue
-
-    if position is None:
-        zero_position += 1
+        return
 
     video_file = source_file
 
-    youtube_data = asmmetadata.get_youtube_info_data(entryinfo)
+    youtube_data = asmmetadata.get_youtube_info_data(entry)
     youtube_title = youtube_data['title']
     description = youtube_data['description']
     category = youtube_data['category']
     tag_list = youtube_data['tags']
-
-    if 'youtube' in entryinfo:
-        print line.encode('utf-8')
-        continue
 
     tags = ",".join(tag_list)
 
@@ -177,15 +98,13 @@ for line in sys.stdin:
             sleep_function(YOUTUBE_MINIMUM_SLEEP_TIME)
         upload_trials += 1
         outlines = call_and_capture_output(args)
-        if 'youtube.com' in outlines[-1]:
+        #sys.stderr.write("%s\n" % outlines)
+        youtube_id = asmyoutube.get_video_id_try_url(outlines[0])
+        if youtube_id:
             upload_success = True
-            youtube_http_id = outlines[-1]
-            youtube_http_id = re.sub(
-                r"^(.+?)https?://www\.youtube\.com/watch\?v=", "", youtube_http_id)
-            youtube_id = "|youtube:" + youtube_http_id
-            failures = 0
+            state.failures = 0
         else:
-            sys.stderr.write(("UPLOAD failed %s\n" % line).encode('utf-8'))
+            sys.stderr.write(("UPLOAD failed %s\n" % youtube_title).encode('utf-8'))
             sys.stderr.write("\n".join(outlines))
             sys.stderr.write(youtube_title.encode('utf-8'))
             sys.stderr.write("\n")
@@ -195,11 +114,59 @@ for line in sys.stdin:
             sys.stderr.write("\n")
 
     if upload_success:
-        failures = 0
+        state.failures = 0
     else:
-        failures += 1
-    print (line + youtube_id).encode('utf-8')
-    sys.stdout.flush()
-    sys.stderr.write("%s - %s done\n" % (youtube_title, youtube_id))
+        state.failures += 1
+
+    if youtube_id is not None:
+        entry["youtube"] = youtube_id
     # 61 seconds delay between sends is OK, 57 is not.
     sleep_function(61)
+
+
+def main(argv):
+    parser = argparse.ArgumentParser(description='Upload videos to Youtube.')
+    parser.add_argument('metadata_file', metavar="metadata-file")
+    parser.add_argument('files_root', metavar="files-root")
+    parser.add_argument('--video-postfix', default=".mp4")
+    parser.add_argument('--dry-run', action="store_true")
+    parser.add_argument('--media-vod-directory')
+    parser.add_argument(
+        '--privacy', default="public", choices=["public", "unlisted", "private"])
+    args = parser.parse_args(argv[1:])
+
+    files_root = args.files_root
+    video_postfix = args.video_postfix
+    media_vod_directory = args.media_vod_directory
+    privacy = args.privacy
+
+    sleep_function = time.sleep
+    if args.dry_run:
+        sleep_function = lambda x : None
+        call_and_capture_output = call_and_capture_output_fake
+    else:
+        call_and_capture_output = call_and_capture_output_real
+
+    metadata = asmmetadata.parse_file(open(args.metadata_file, "rb"))
+
+    state = State(
+        files_root=files_root,
+        media_vod_directory=media_vod_directory)
+
+    try:
+        for entry in metadata.entries:
+            handle_entry(
+                call_and_capture_output, sleep_function, privacy, state, entry)
+    except:
+        asmmetadata.print_metadata(
+            open(args.metadata_file, "wb"), metadata)
+        raise
+    asmmetadata.print_metadata(
+        open(args.metadata_file, "wb"), metadata)
+    if args.dry_run:
+        return 1
+    return os.EX_OK
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
--- upload-to-youtube.sh
+++ upload-to-youtube.sh
@@ -8,5 +8,4 @@ DATAFILE="${1:-$DATAFILE}"
 DATAFILE_TMP="$(basename "$DATAFILE" .txt)-$(date +%Y%m%d%H%M%S).txt"
 
 "${PYTHON[@]}" lib/upload-to-youtube-video.py "${YOUTUBE_OPTIONS[@]}" \
-    --media-vod-directory="$FILES_MAO" "$FILES_ROOT" < "$DATAFILE" > "$DATAFILE_TMP"
-cat "$DATAFILE_TMP" > "$DATAFILE"
+    --media-vod-directory="$FILES_MAO" "$DATAFILE" "$FILES_ROOT"
