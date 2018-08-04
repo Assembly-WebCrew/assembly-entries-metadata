7f0abd1817b02f49299908bdfd8be82cbc1a2b9c
Jussi Judin <jjudin+github@iki.fi>
2018-08-04 16:15:03 +0300

    Add pixel graphics, photo, some assemblytv, tracked music, and esports

--- convert-music-to-video.sh
+++ convert-music-to-video.sh
@@ -8,15 +8,23 @@ CATEGORY="$1"
 
 cd "$FILES_ROOT" || exit 1
 test -d "$CATEGORY" || exit 1
-test -f "$MUSIC_BACKGROUND" || exit 1
+# Execute music-background-to-long-video.sh if this file is
+# missing.
+test -f "$MUSIC_BACKGROUND".mp4 || exit 1
 
 for MUSIC_FILE in "$CATEGORY"/{*.wav,*.ogg,*.mp3,*.flac,*.aif,*.aiff}; do
     [[ -f "$MUSIC_FILE" ]] || continue
     TARGET_FILE="$MUSIC_FILE".mp4
 
-    "${FFMPEG[@]}" -loop 1 -r 10 \
-        -i "$MUSIC_BACKGROUND" -i "$MUSIC_FILE" \
+    # "${FFMPEG[@]}" -loop 1 -r 10 \
+    #     -i "$MUSIC_BACKGROUND" -i "$MUSIC_FILE" \
+    #     -c:a aac -b:a 384k \
+    #     -c:v libx264 -r 10 -crf 0 -preset ultrafast -shortest \
+    #     -y "$TARGET_FILE"
+    "${FFMPEG[@]}" \
+        -i "$MUSIC_BACKGROUND".mp4 -i "$MUSIC_FILE" \
+        -shortest \
         -c:a aac -b:a 384k \
-        -c:v libx264 -r 10 -crf 0 -preset ultrafast -shortest \
+        -c:v copy \
         -y "$TARGET_FILE"
 done
