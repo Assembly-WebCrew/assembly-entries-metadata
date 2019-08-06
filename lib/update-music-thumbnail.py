79cad547fd5a75a1e0db56dc4c5193fa64630ffa
Jussi Judin <jjudin+github@iki.fi>
2019-08-07 01:06:32 +0300

    Also worsen music thumbnail quality

--- lib/update-music-thumbnail.py
+++ lib/update-music-thumbnail.py
@@ -14,18 +14,29 @@ def main(argv):
     parser.add_argument("thumbnails_dir")
     args = parser.parse_args(argv[1:])
 
+    convert_params = [
+        "-sampling-factor",
+        "4:2:0",
+        "-strip",
+        "-quality",
+        "85",
+        "-interlace",
+        "JPEG",
+        "-colorspace",
+        "sRGB"]
     base_width = 160
 
     multipliers = [1.25, 1.5, 2, 3, 4]
     base_height = int(base_width * 9 / 16)
     size_default = archivethumbnails.ImageSize(
-        base_width, base_height)
+        base_width, base_height, convert_params)
     extra_sizes = []
     for multiplier in multipliers:
         extra_width = int(size_default.x * multiplier)
         extra_height = int(size_default.x * multiplier * 9 / 16)
         extra_sizes.append(
-            archivethumbnails.ImageSize(extra_width, extra_height))
+            archivethumbnails.ImageSize(
+                extra_width, extra_height, convert_params))
 
     filename = args.background_file
     out_prefix = os.path.join(args.thumbnails_dir, "music-thumbnail")
