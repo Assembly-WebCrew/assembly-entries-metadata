1d010e4b53683a99748a48cb4819891061ef1b92
Jussi Judin <jjudin+github@iki.fi>
2019-09-22 16:54:26 +0300

    Update facedetect to print out detection data

--- lib/facedetect.cpp
+++ lib/facedetect.cpp
@@ -1,195 +1,91 @@
-#include "opencv2/objdetect/objdetect.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/objdetect.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
 
 #include <iostream>
-#include <stdio.h>
-#include <ostream>
 
-using namespace std;
-using namespace cv;
+typedef struct {
+    cv::Mat frame;
+    cv::CascadeClassifier face_cascade;
+    cv::CascadeClassifier eyes_cascade;
+} ImageInput;
 
-void help()
+static cv::Point detect_weightiest_face(ImageInput& input)
 {
-    cout << "\nThis program demonstrates the cascade recognizer. Now you can use Haar or LBP features.\n"
-            "This classifier can recognize many ~rigid objects, it's most known use is for faces.\n"
-            "Usage:\n"
-            "./facedetect [--cascade=<cascade_path> this is the primary trained classifier such as frontal face]\n"
-               "   [--nested-cascade[=nested_cascade_path this an optional secondary classifier such as eyes]]\n"
-               "   [--scale=<image scale greater or equal to 1, try 1.3 for example>\n"
-               "   [filename|camera_index]\n\n"
-            "see facedetect.cmd for one call:\n"
-            "./facedetect --cascade=\"../../data/haarcascades/haarcascade_frontalface_alt.xml\" --nested-cascade=\"../../data/haarcascades/haarcascade_eye.xml\" --scale=1.3 \n"
-            "Hit any key to quit.\n"
-            "Using OpenCV version " << CV_VERSION << "\n" << endl;
+    cv::Mat frame_gray = input.frame;
+    equalizeHist(frame_gray, frame_gray);
+
+    //-- Detect faces
+    std::vector<cv::Rect> faces;
+    input.face_cascade.detectMultiScale(frame_gray, faces);
+
+    size_t max_score = 0;
+    int max_index = -1;
+    for (int i = 0; i < faces.size(); i++) {
+        const auto face = faces[i];
+        size_t face_score = face.width * face.height;
+        cv::Mat faceROI = frame_gray(faces[i]);
+
+        //-- In each face, detect eyes
+        std::vector<cv::Rect> eyes;
+        input.eyes_cascade.detectMultiScale(faceROI, eyes);
+        face_score *= (1 + eyes.size());
+
+        if (face_score > max_score) {
+            max_score = face_score;
+            max_index = i;
+        }
+    }
+    if (max_index == -1) {
+        return {input.frame.size().width / 2, input.frame.size().height / 2};
+    }
+    const auto max_face = faces[max_index];
+    return {max_face.x + max_face.width/2, max_face.y + max_face.height/2};
 }
-
-void detectAndDraw( Mat& img,
-                   CascadeClassifier& cascade, CascadeClassifier& nestedCascade,
-                   double scale);
-
-String cascadeName = "../../data/haarcascades/haarcascade_frontalface_alt.xml";
-String nestedCascadeName = "../../data/haarcascades/haarcascade_eye_tree_eyeglasses.xml";
 
 int main( int argc, const char** argv )
 {
-    CvCapture* capture = 0;
-    Mat frame, frameCopy, image;
-    const String scaleOpt = "--scale=";
-    size_t scaleOptLen = scaleOpt.length();
-    const String cascadeOpt = "--cascade=";
-    size_t cascadeOptLen = cascadeOpt.length();
-    const String nestedCascadeOpt = "--nested-cascade";
-    size_t nestedCascadeOptLen = nestedCascadeOpt.length();
-    String inputName;
+    cv::CommandLineParser parser(
+        argc, argv,
+        "{help h||}"
+        "{image|<none>|Image file.}"
+        "{face_cascade|haarcascade_frontalface_alt.xml|Path to face cascade.}"
+        "{eyes_cascade|haarcascade_eye_tree_eyeglasses.xml|Path to eyes cascade.}"
+        );
 
-    //help();
+    parser.about( "\nThis program demonstrates using the cv::CascadeClassifier class to detect objects (Face + eyes) in a video stream.\n"
+                  "You can use Haar or LBP features.\n\n" );
 
-    CascadeClassifier cascade, nestedCascade;
-    double scale = 1;
+    const auto face_cascade_name = parser.get<cv::String>("face_cascade");
+    const auto eyes_cascade_name = parser.get<cv::String>("eyes_cascade");
+    const auto image_name = parser.get<cv::String>("image");
 
-    for( int i = 1; i < argc; i++ )
+    cv::CascadeClassifier face_cascade;
+    if (!face_cascade.load(face_cascade_name))
     {
-        //cout << "Processing " << i << " " <<  argv[i] << endl;
-        if( cascadeOpt.compare( 0, cascadeOptLen, argv[i], cascadeOptLen ) == 0 )
-        {
-            cascadeName.assign( argv[i] + cascadeOptLen );
-            //cout << "  from which we have cascadeName= " << cascadeName << endl;
-        }
-        else if( nestedCascadeOpt.compare( 0, nestedCascadeOptLen, argv[i], nestedCascadeOptLen ) == 0 )
-        {
-            if( argv[i][nestedCascadeOpt.length()] == '=' )
-                nestedCascadeName.assign( argv[i] + nestedCascadeOpt.length() + 1 );
-            if( !nestedCascade.load( nestedCascadeName ) )
-                cerr << "WARNING: Could not load classifier cascade for nested objects" << endl;
-        }
-        else if( scaleOpt.compare( 0, scaleOptLen, argv[i], scaleOptLen ) == 0 )
-        {
-            if( !sscanf( argv[i] + scaleOpt.length(), "%lf", &scale ) || scale < 1 )
-                scale = 1;
-            //cout << " from which we read scale = " << scale << endl;
-        }
-        else if( argv[i][0] == '-' )
-        {
-            cerr << "WARNING: Unknown option %s" << argv[i] << endl;
-        }
-        else
-            inputName.assign( argv[i] );
+        std::cerr << "Error loading face cascade from " << face_cascade_name << "\n";
+        return EXIT_FAILURE;
+    };
+    cv::CascadeClassifier eyes_cascade;
+    if( !eyes_cascade.load( eyes_cascade_name ) )
+    {
+        std::cerr << "Error loading eyes cascade from " << eyes_cascade_name << "\n";
+        return EXIT_FAILURE;
+    };
+
+    auto image = cv::imread(image_name, cv::IMREAD_GRAYSCALE);
+    if (image.data == NULL) {
+        std::cerr << "Failed to read image from " << image_name << "\n";
+        return EXIT_FAILURE;
     }
 
-    if( !cascade.load( cascadeName ) )
-    {
-        cerr << "ERROR: Could not load classifier cascade" << endl;
-        cerr << "Usage: facedetect [--cascade=<cascade_path>]\n"
-            "   [--nested-cascade[=nested_cascade_path]]\n"
-            "   [--scale[=<image scale>\n"
-            "   [filename|camera_index]\n" << endl ;
-        return -1;
-    }
-
-    if( inputName.empty() || (isdigit(inputName.c_str()[0]) && inputName.c_str()[1] == '\0') )
-    {
-        capture = cvCaptureFromCAM( inputName.empty() ? 0 : inputName.c_str()[0] - '0' );
-        int c = inputName.empty() ? 0 : inputName.c_str()[0] - '0' ;
-        if(!capture) cout << "Capture from CAM " <<  c << " didn't work" << endl;
-    }
-    else if( inputName.size() )
-    {
-        image = imread( inputName, 1 );
-        if( image.empty() )
-        {
-            capture = cvCaptureFromAVI( inputName.c_str() );
-            if(!capture) cout << "Capture from AVI didn't work" << endl;
-        }
-    }
-    else
-    {
-        image = imread( "lena.jpg", 1 );
-        if(image.empty()) cout << "Couldn't read lena.jpg" << endl;
-    }
-
-    //cout << "In image read" << endl;
-    if( !image.empty() )
-    {
-        detectAndDraw( image, cascade, nestedCascade, scale );
-    }
-
-    return 0;
+    ImageInput input{image, face_cascade, eyes_cascade};
+    const auto face_center = detect_weightiest_face(input);
+    std::cout
+        << image.size().width << " "
+        << image.size().height << " "
+        << face_center.x << " "
+        << face_center.y << "\n";
+    return EXIT_SUCCESS;
 }
 
-std::string getFaceLocationString(const Rect& r, const double scale)
-{
-    std::ostringstream result;
-    result << cvRound(r.x*scale) << "," << cvRound(r.y*scale) << "," << cvRound(r.width*scale) << "," << cvRound(r.height*scale);
-    return result.str();
-}
-
-
-void detectAndDraw(Mat& img,
-                   CascadeClassifier& cascade, CascadeClassifier& nestedCascade,
-                   double scale)
-{
-    int i = 0;
-    double t = 0;
-    vector<Rect> faces;
-    const static Scalar colors[] =  { CV_RGB(0,0,255),
-        CV_RGB(0,128,255),
-        CV_RGB(0,255,255),
-        CV_RGB(0,255,0),
-        CV_RGB(255,128,0),
-        CV_RGB(255,255,0),
-        CV_RGB(255,0,0),
-        CV_RGB(255,0,255)} ;
-    Mat gray, smallImg( cvRound (img.rows/scale), cvRound(img.cols/scale), CV_8UC1 );
-
-    cvtColor( img, gray, CV_BGR2GRAY );
-    resize( gray, smallImg, smallImg.size(), 0, 0, INTER_LINEAR );
-    equalizeHist( smallImg, smallImg );
-
-    std::cout << img.cols << "x" << img.rows;
-
-    t = (double)cvGetTickCount();
-    cascade.detectMultiScale( smallImg, faces,
-        1.1, 2, 0
-        //|CV_HAAR_FIND_BIGGEST_OBJECT
-        //|CV_HAAR_DO_ROUGH_SEARCH
-        |CV_HAAR_SCALE_IMAGE
-        ,
-        Size(30, 30) );
-    t = (double)cvGetTickCount() - t;
-    //printf( "detection time = %g ms\n", t/((double)cvGetTickFrequency()*1000.) );
-    for( vector<Rect>::const_iterator r = faces.begin(); r != faces.end(); r++, i++ )
-    {
-        Mat smallImgROI;
-        vector<Rect> nestedObjects;
-        Point center;
-        Scalar color = colors[i%8];
-        int radius;
-        center.x = cvRound((r->x + r->width*0.5)*scale);
-        center.y = cvRound((r->y + r->height*0.5)*scale);
-        std::cout << " " << getFaceLocationString(*r, scale);
-        radius = cvRound((r->width + r->height)*0.25*scale);
-        circle( img, center, radius, color, 3, 8, 0 );
-        if( nestedCascade.empty() )
-            continue;
-        smallImgROI = smallImg(*r);
-        nestedCascade.detectMultiScale( smallImgROI, nestedObjects,
-            1.1, 2, 0
-            //|CV_HAAR_FIND_BIGGEST_OBJECT
-            //|CV_HAAR_DO_ROUGH_SEARCH
-            //|CV_HAAR_DO_CANNY_PRUNING
-            |CV_HAAR_SCALE_IMAGE
-            ,
-            Size(30, 30) );
-        for( vector<Rect>::const_iterator nr = nestedObjects.begin(); nr != nestedObjects.end(); nr++ )
-        {
-            center.x = cvRound((r->x + nr->x + nr->width*0.5)*scale);
-            center.y = cvRound((r->y + nr->y + nr->height*0.5)*scale);
-            std::cout << " " << getFaceLocationString(*r, scale);
-            radius = cvRound((nr->width + nr->height)*0.25*scale);
-            circle( img, center, radius, color, 3, 8, 0 );
-        }
-    }
-    std::cout << "\n";
-    // cv::imshow( "result", img );
-}
